eb6da15f1a15d6d55b183bc43eed81ecc6f3e410
MericGit <41242144+MericGit@users.noreply.github.com>
2021-08-30 10:12:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+import React from 'react';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEES1eIh2bXMM1wgAAPAfMkoWDqshcbCk",
+  authDomain: "nitrate-eccc5.firebaseapp.com",
+  projectId: "nitrate-eccc5",
+  storageBucket: "nitrate-eccc5.appspot.com",
+  messagingSenderId: "234683029901",
+  appId: "1:234683029901:web:3ca449053972f8769745c4",
+  measurementId: "G-JZ1K8GKGNY"
+};
+
+firebase.initializeApp({
+  firebaseConfig 
+})
+
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
