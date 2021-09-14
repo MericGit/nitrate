6eb9bf64ffa9e5f6812dee474445f86c5235d7e0
MericGit <41242144+MericGit@users.noreply.github.com>
2021-09-14 09:54:17 -0500

    react ex

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import reactDom from 'react-dom';
+import "./index.css"; 
 
+const image = ""
+const title = ""
+const desc = ""
+const date = ""
 
-function Greeting(){
-  return <h4>test maoogsus</h4>;
+function App(){
+  return (
+    <section class = "events">
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
+
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
+    </section>
+  )
 }
 
-reactDom.render(<Greeting/>,document.getElementById('root'));
+const Event = (props) => {
+  return (
+    <article className ='event'>
+      <img src={props.image} alt =''/>
+      <h1>{props.title}</h1>
+      <h3>{props.date}</h3>
+      <h4>{props.desc}</h4>
+    </article>
+  );
+};
+reactDom.render(<App/>,document.getElementById('root'));
+
 
