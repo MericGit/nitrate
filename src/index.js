ea5bff774038fe1f90b15311cacd562b5fa64834
MericGit <41242144+MericGit@users.noreply.github.com>
2021-09-14 10:17:46 -0500

    Amoogus website update

--- src/index.js
+++ src/index.js
@@ -9,24 +9,24 @@ const date = ""
 
 function App(){
   return (
-    <section class = "events">
-      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
-
-      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
-      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
-      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
-      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest!"/>
+    <section class = "events">    
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/5/2021" desc = "Clean up the forest! and play among us too"/>
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/23/2021" desc = "Clean up the forest! and then vent through the forest"/>
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "9/4/2021" desc = "Clean up the forest! and find the imposters"/>
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "10/31/2021" desc = "Clean up the forest! and call emergency meetings"/>
+      <Event image="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4959784.jpg" title = "Forest Cleanup" date = "4/6/2021" desc = "Clean up the forest! sus! sus!"/>
     </section>
   )
 }
 
 const Event = (props) => {
+  const {image, title, date, desc} = props
   return (
     <article className ='event'>
-      <img src={props.image} alt =''/>
-      <h1>{props.title}</h1>
-      <h3>{props.date}</h3>
-      <h4>{props.desc}</h4>
+      <img src={image} alt =''/>
+      <h1>{title}</h1>
+      <h3>{date}</h3>
+      <h4>{desc}</h4>
     </article>
   );
 };
